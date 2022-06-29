a45cdf04b9a2882b27a57a26dc13c4595d6a33e8
mozaicdb <mozaic138@gmail.com>
2022-06-28 20:58:29 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define a class named Square"""
+
+
+class Square:
+    """Square Classe"""
+    def __init__(self, size):
+        """
+        Init a square
+        Args:
+            size (int): Size of the square
+        Returns: None
+        """
+        self.__size = size
