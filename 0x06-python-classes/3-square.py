176d6cde59299537dac02451d92e64e90fda5036
mozaicdb <mozaic138@gmail.com>
2022-06-28 21:06:01 -0700

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""Define a class named Square"""
+
+
+class Square:
+    """Square Classe"""
+    def __init__(self, size=0):
+        """
+        Init a square
+        Args:
+            size (int): Size of the square
+        Raises:
+            TypeError: Error if not an int
+            ValueError: Error if not positiv
+        Returns: None
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calcul the area of the square
+        Returns: The area of the square
+        """
+        return self.__size ** 2
