160e34dbc01bb9a07bafd1549e273739ae13c74c
mozaicdb <mozaic138@gmail.com>
2022-07-05 10:49:23 -0700

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Convert a obj into json file
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Convert a obj into json file
+    """
+    return json.dumps(my_obj)
