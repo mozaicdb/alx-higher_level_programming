95e529be813af23b5f1a2790b6019363659439f1
mozaicdb <mozaic138@gmail.com>
2023-10-25 11:28:14 -0700

    100-safe

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-
-import sys
-
 def safe_print_integer_err(value):
-    """ This function prints an integer with :d format"""
+    import sys
     try:
         print("{:d}".format(value))
-        return (True)
-    except  (TypeError, ValueError):
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (False)
+        return True
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return False
