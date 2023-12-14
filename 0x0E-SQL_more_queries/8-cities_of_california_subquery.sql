4c4aeed033e7c8633fd0b855fae1a3bce7963755
mozaicdb <mozaic138@gmail.com>
2023-12-14 10:31:02 +0000

    corrected

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
-sts all the cities of California that can be found in the database hbtn_0d_usa.
--- The states table contains only one record where name = California
--- Results must be sorted in ascending order by cities.id
--- You are not allowed to use the JOIN keyword
--- The database name will be passed as an argument of the mysql command
-
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id;
+-- Script that lists all the cities of California registered in the database
+SELECT id, name -- Query to list all the cities from California
+FROM cities
+WHERE state_id = ( -- Query to get the id of California
+      SELECT id
+      FROM states
+      WHERE name = "California");
