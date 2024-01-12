986a9b68eae9d4218b00b299c1730ce529a73371
mozaicdb <mozaic138@gmail.com>
2024-01-12 15:03:27 +0000

    square 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,10 @@
-t size = Math.floor(Number(process.argv[2]));
-if (isNaN(size)) {
+#!/usr/bin/node
+// prints a square
+
+if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-  for (let r = 0; r < size; r++) {
-    let row = '';
-    for (let c = 0; c < size; c++) row += 'X';
-    console.log(row);
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log('X'.repeat(parseInt(process.argv[2])));
   }
 }
