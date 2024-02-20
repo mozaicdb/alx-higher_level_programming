402084e80a32b27abded359cffb1432206e41c96
mozaicdb <mozaic138@gmail.com>
2024-02-20 12:59:13 +0000

    done

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,49 @@
 #!/usr/bin/node
 
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request.get(url, (error, response, body) => {
+/*
+let characters = [];
+let dict = {};
+function addToDict (url, name) {
+  dict[url] = name;
+}
+request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
   if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
-      });
-    }
+    console.error(error);
   }
+  characters = JSON.parse(body).characters
+  characters.forEach(function (url) {
+    request(url, function (error, response, body) {
+      if (error) {
+        console.error(error);
+      }
+      addToDict(url, JSON.parse(body).name);
+    });
+  });
+  characters.forEach(function (item) {
+    console.log(dict[item]);
+  })
+});
+*/
+
+function helpRequest (arr, i) {
+  if (i === arr.length) {
+    return;
+  }
+  request(arr[i], function (error, response, body) {
+    if (error) {
+      console.error(error);
+    }
+    console.log(JSON.parse(body).name);
+    helpRequest(arr, i + 1);
+  });
+}
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const charac = JSON.parse(body).characters;
+  helpRequest(charac, 0);
 });
